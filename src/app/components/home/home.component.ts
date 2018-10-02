b6aaa7c87857f2be287e738ed5e386e24547f19f
Enrico Lister <enricolister@hotmail.com>
2018-10-03 00:20:34 +0200

    - Tested HTTP get functionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient ) {
+
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe( (results: any) => { // Deve essere dichiarato di tipo any altrimenti results sarebbe di tipo object e non assegnabile ad una variabile any
+      this.paises = results;
+      console.log(results);
+    });
+  }
 
   ngOnInit() {
   }
