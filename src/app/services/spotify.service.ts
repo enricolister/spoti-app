eba2ad59ed73d685aa4edd2c01d45989f1d3ff09
Enrico Lister <enricolister@hotmail.com>
2018-10-06 00:27:53 +0200

    waaaa

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,12 +12,18 @@ export class SpotifyService {
   }
 
   getToken() {
+    const body = new HttpParams()
+      .set('grant_type', 'client_credentials')
+      .set('client_id', 'dbc5cf4ec79546b7a9df8e6ddaa8d8f9')
+      .set('client_secret', '6e2ddb28acba453da7ce8cb6407b6965');
+
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
       })
     };
-    this.http.post('https://accounts.spotify.com/api/token', {'grant_type': 'client_credentials', 'client_id': 'dbc5cf4ec79546b7a9df8e6ddaa8d8f9', 'client_secret': '6e2ddb28acba453da7ce8cb6407b6965'}, httpOptions)
+
+    this.http.post('https://accounts.spotify.com/api/token', body.toString(), httpOptions)
     .subscribe( (data: any) => {
       console.log(data.access_token);
       return data.access_token;
@@ -26,7 +32,7 @@ export class SpotifyService {
 
   getNewRelases() {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + 'BQAwIL0Xikd-brkB6wNzbkE76Nyn2V4NTeKF47oBEEETKSno6XkejwpPoGjP_Pz4N3giXXbk1VQco1OsR5E'
+      'Authorization': 'Bearer ' + this.getToken()
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
