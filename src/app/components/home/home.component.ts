7628e790812b32d4d59dc3d66467391be517375c
enricolister@hotmail.com <enricolister@hotmail.com>
2018-10-06 19:28:42 +0200

    Get token to work

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,13 +9,19 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class HomeComponent implements OnInit {
 
+  token;
   nuevasCanciones: any[] = [];
 
   constructor( private spotify: SpotifyService ) {
-    this.spotify.getNewRelases()
+    this.spotify.getToken()
     .subscribe( (data: any) => {
-      console.log(data.albums.items);
-      this.nuevasCanciones = data.albums.items;
+      this.token = data.access_token;
+
+      this.spotify.getNewRelases(this.token)
+    .subscribe( (data2: any) => {
+      console.log(data2.albums.items);
+      this.nuevasCanciones = data2.albums.items;
+    });
     });
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
-
-  token: string;
 
   constructor( private http: HttpClient) {
     console.log('Spotify Service ready.');
@@ -19,7 +18,7 @@ export class SpotifyService {
     const body = new HttpParams()
       .set('grant_type', 'client_credentials')
       .set('client_id', 'dbc5cf4ec79546b7a9df8e6ddaa8d8f9')
-      .set('client_secret', '6e2ddb28acba453da7ce8cb6407b6965');
+      .set('client_secret', '');
 
     const httpOptions = {
       headers: new HttpHeaders({
@@ -27,21 +26,14 @@ export class SpotifyService {
       })
     };
 
-    this.http.post('https://accounts.spotify.com/api/token', body.toString(), httpOptions)
-    .subscribe( (data: any) => {
-      this.token = data.access_token;
-      // console.log(this.token);
-    });
+    return this.http.post('https://accounts.spotify.com/api/token', body.toString(), httpOptions);
   }
 
-  getNewRelases() {
-    setTimeout(() => {
-    console.log('Token che usero ' + this.token);
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.token
-    });
-      return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
-    }, 1000);
-
+  getNewRelases(token) {
+        console.log('Token che usero ' + token);
+        const headers = new HttpHeaders({
+          'Authorization': 'Bearer ' + token
+        });
+        return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
 }
